e46014e7628aeeef23a64fad129d395a7cd7309a
klebermmaia <klebermmaia@gmail.com>
2022-10-02 15:57:02 -0300

    novas funções

--- 1-crud_com_javascript/script.js
+++ 1-crud_com_javascript/script.js
@@ -1,3 +1,4 @@
+
 class Cadastro {
   constructor() {
     this.id;
@@ -7,18 +8,43 @@ class Cadastro {
     const people = this.readData();
     if(this.checkData(people)){
       this.addPeople(people);
-      this.writeOnScreen()
+      this.writeOnScreen();
+      this.setData();
     }
   }
+  delete(event){
+    event.preventDefault();
+    const lines = document.querySelectorAll('[data-people]');
+    const linesRegistered = document.querySelector('[data-registered]')
+    lines.forEach((line)=>{
+      // console.log(event.target.dataset['delete'])
+      if(line.dataset['people'] == this.dataset['delete']){
+        let i = line.dataset['people'];
+        console.log(registered.registeresPeople.splice(i, 1))
+        registered.registeresPeople.splice(i, 1);
+        linesRegistered.deleteRow(i);
+      }
+    });
+  }
+  setData(){
+    localStorage.setItem('registeres_people', JSON.stringify(this.registeresPeople));
+  }
+  loadData(){
+    const registeres_people = JSON.parse(localStorage.getItem('registeres_people')) ?? [];
+    for (let i in registeres_people){
+      this.registeresPeople.push(registeres_people[i]);
+    }
+    this.writeOnScreen(registeres_people);
+  }
   writeOnScreen(){
-    const registered = document.querySelector('[data-registered');
-    registered.innerHTML = ''
+    const registered = document.querySelector('[data-registered]');
+    registered.innerHTML = '';
+
     for(let i = 0; i < this.registeresPeople.length; i++){
       let name = this.registeresPeople[i].name;
       let email = this.registeresPeople[i].email;
       let phone = this.registeresPeople[i].phone;
       let birth = this.registeresPeople[i].birth;
-
       const newTr = document.createElement('tr');
       newTr.dataset.people = `${i}`;
       registered.insertAdjacentElement("afterbegin", newTr);
@@ -30,12 +56,19 @@ class Cadastro {
         <td>${birth}</td>
         <td>
           <div class="actions">
-            <button type='button' class='btn btn-warning px-2 py-1'>Editar</button>
-            <button type='button' class='btn btn-danger px-2 py-1'>Deletar</button>
+            <button type='button' class='btn btn-warning px-2 py-1' date-edit>Editar</button>
+            <button type='button' class='btn btn-danger px-2 py-1' data-delete="${i}">Deletar</button>
           </div>
         </td>
       `);
     }
+    
+    const btnDelete = document.querySelectorAll('[data-delete]');
+    btnDelete.forEach(btn => {
+      ['click'].forEach(event =>{
+          btn.addEventListener(event, this.delete);
+      })
+    })
   }
   addPeople(people){
     this.registeresPeople.push(people);
@@ -44,8 +77,8 @@ class Cadastro {
   addIds(){
     let id = 0;
     for(let person of this.registeresPeople){
-      id++
       person.id = id;
+      id++
     }
   }
   checkData(people){
@@ -79,3 +112,8 @@ class Cadastro {
   }
 }
 const registered = new Cadastro();
+
+window.addEventListener('load', ()=> {
+  registered.loadData()
+})
+
